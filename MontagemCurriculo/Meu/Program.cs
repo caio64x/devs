c4a5da343eebe897fed248a9222fc1dfc54ab752
Caio64x <caiolimeira10@hotmail.com>
2021-03-11 13:49:07 -0300

    pontapé

---
+++ MontagemCurriculo/Meu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Meu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MontagemCurriculo/MontagemCurriculo/Mapeamento/IdiomaMap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MontagemCurriculo.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MontagemCurriculo.Mapeamento
+{
+    public class IdiomaMap : IEntityTypeConfiguration<Idioma>
+    {
+        public void Configure(EntityTypeBuilder<Idioma> builder)
+        {
+            //chave primaria
+            builder.HasKey(i => i.IdiomaID);
+
+            //propriedade comum
+            builder.Property(i => i.Nome).IsRequired().HasMaxLength(50);
+            //informacao unica
+            builder.HasIndex(i => i.Nome).IsUnique();
+            //propriedade comum
+            builder.Property(i => i.Nivel).IsRequired().HasMaxLength(50);
+
+            //tem um curriculo com varios idiomas
+            builder.HasOne(i => i.Curriculo).WithMany(i => i.Idiomas).HasForeignKey(i => i.CurriculoID);
+
+            builder.ToTable("Idiomas");
+        }
+    }
+}
