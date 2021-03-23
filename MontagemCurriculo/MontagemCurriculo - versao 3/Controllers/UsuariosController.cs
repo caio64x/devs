01b80031a977166afb38acffd8ecfae733080fe5
Caio64x <caiolimeira10@hotmail.com>
2021-03-23 16:39:19 -0300

    versao 3.1 implementação da tela de login separada da tela de cadastro/registro

--- MontagemCurriculo/MontagemCurriculo - versao 3/Controllers/UsuariosController.cs
+++ MontagemCurriculo/MontagemCurriculo - versao 3/Controllers/UsuariosController.cs
@@ -13,33 +13,78 @@ using MontagemCurriculo.ViewModels;
 
 namespace MontagemCurriculo.Controllers
 {
-    public class UsuariosController : Controller
+public class UsuariosController : Controller
+{
+    private readonly Contexto _context;
+
+    public UsuariosController(Contexto context)
     {
-        private readonly Contexto _context;
+        _context = context;
+    }
 
-        public UsuariosController(Contexto context)
-        {
-            _context = context;
-        }
+    public IActionResult Registro()
+    {
+        return View();
+    }
 
-        public IActionResult Registro()
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Registro([Bind("UsuarioID,Email,Senha")] Usuario usuario)
+    {
+        if (ModelState.IsValid)
         {
-            return View();
-        }
+            //adiciona e salva no contexto do banco de dados
+            _context.Add(usuario);
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Registro([Bind("UsuarioID,Email,Senha")] Usuario usuario)
-        {
-            if (ModelState.IsValid)
+            //bloco de dados de informacoes
+            InformacaoLogin informacao = new InformacaoLogin
             {
-                //adiciona e salva no contexto do banco de dados
-                _context.Add(usuario);
-               
-                //bloco de dados de informacoes
+                UsuarioID = usuario.UsuarioID,
+                EnderecoIP = Request.HttpContext.Connection.RemoteIpAddress.ToString(),
+                Data = DateTime.Now.ToShortDateString(),
+                Horario = DateTime.Now.ToShortTimeString()
+            };
+
+            _context.Add(informacao);
+            await _context.SaveChangesAsync();
+
+            HttpContext.Session.SetInt32("UsuarioID", usuario.UsuarioID);
+
+            var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Email, usuario.Email)
+                };
+
+            var userIdentity = new ClaimsIdentity(claims, "login");
+            ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+            await HttpContext.SignInAsync(principal);
+
+            return RedirectToAction("Index", "Curriculos");
+        }
+        return View(usuario);
+    }
+                //força deslogar em caso de confusao 
+    [HttpGet]
+    public IActionResult Login()
+    {
+        if (User.Identity.IsAuthenticated)
+            HttpContext.Session.Clear();
+
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel login)
+    {
+        if (ModelState.IsValid)
+        {
+            if (_context.Usuarios.Any(u => u.Email == login.Email && u.Senha == login.Senha))
+            {
+                int ID = _context.Usuarios.Where(u => u.Email == login.Email && u.Senha == login.Senha).Select(u => u.UsuarioID).Single();
+
                 InformacaoLogin informacao = new InformacaoLogin
                 {
-                    UsuarioID = usuario.UsuarioID,
+                    UsuarioID = ID,
                     EnderecoIP = Request.HttpContext.Connection.RemoteIpAddress.ToString(),
                     Data = DateTime.Now.ToShortDateString(),
                     Horario = DateTime.Now.ToShortTimeString()
@@ -48,78 +93,36 @@ namespace MontagemCurriculo.Controllers
                 _context.Add(informacao);
                 await _context.SaveChangesAsync();
 
-                HttpContext.Session.SetInt32("UsuarioID", usuario.UsuarioID);
+                HttpContext.Session.SetInt32("UsuarioID", ID);
 
                 var claims = new List<Claim>
-                {
-                    new Claim(ClaimTypes.Email, usuario.Email)
-                };
-
-                //joga o ID do usuario em uma sessao para futuro uso
-                var userIdentity = new ClaimsIdentity(claims, "login");
-                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
-                await HttpContext.SignInAsync(principal);
-
-                //retorna o index de curriculos
-                return RedirectToAction("Index", "Curriculos");
-            }
-            return View(usuario);
-        }
-
-        //força deslogar em caso de confusao 
-        [HttpHead]
-        public IActionResult Login()
-        {
-            if (User.Identity.IsAuthenticated)
-                HttpContext.Session.Clear();
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel login)
-        {
-            if (ModelState.IsValid)
-            {
-                if (_context.Usuarios.Any(u => u.Email == login.Email && u.Senha == login.Senha))
-                {
-                    int ID = _context.Usuarios.Where(u => u.Email == login.Email && u.Senha == login.Senha).Select(u => u.UsuarioID).Single();
-
-                    InformacaoLogin informacao = new InformacaoLogin
-                    {
-                        UsuarioID = ID,
-                        EnderecoIP = Request.HttpContext.Connection.RemoteIpAddress.ToString(),
-                        Data = DateTime.Now.ToShortDateString(),
-                        Horario = DateTime.Now.ToShortTimeString()
-                    };
-
-                    _context.Add(informacao);
-                    await _context.SaveChangesAsync();
-
-                    HttpContext.Session.SetInt32("UsuarioID", ID);
-
-                    var claims = new List<Claim>
                     {
                         new Claim(ClaimTypes.Email, login.Email)
                     };
 
-                    var userIdentity = new ClaimsIdentity(claims, "login");
-                    ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
-                    await HttpContext.SignInAsync(principal);
+                var userIdentity = new ClaimsIdentity(claims, "login");
+                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+                await HttpContext.SignInAsync(principal);
 
-                    return RedirectToAction("Index", "Curriculos");
+                return RedirectToAction("Index", "Curriculos");
                 }
+
             }
-
-            return View(login);
+            return View(login);         
         }
 
-
-
-        public JsonResult UsuarioExiste(string Email)
-        {
-            if (!_context.Usuarios.Any(e => e.Email == Email))
-                return Json(true);
-            return Json("Email existente");
-        }
+        public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync();
+        HttpContext.Session.Clear();
+        return RedirectToAction("Login", "Usuarios");
     }
+
+    public JsonResult UsuarioExiste(string Email)
+    {
+        if (!_context.Usuarios.Any(e => e.Email == Email))
+            return Json(true);
+        return Json("Email existente");
+    }
+}
 }
